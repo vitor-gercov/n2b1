1c4990dacf75776c0eb9a446870889dba9a0a5cf
Vitor Gobato Gercov <gercovvitor@gmail.com>
2022-09-14 19:10:15 -0300

    registro de categorias implementado

--- screens/foodCategories/FoodCategoriesScreen.tsx
+++ screens/foodCategories/FoodCategoriesScreen.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import { RootTabScreenProps } from '../../types';
+import * as databaseService from '../../services/databaseService'
+import { FoodCategory } from '../../models';
 
 export default function FoodCategoriesScreen({ navigation }: RootTabScreenProps<'FoodCategories'>) {
+  const [foodCategories, setFoodCategories] = useState([new FoodCategory()])
+
+  // function getFoodCategories(): void {
+  //   databaseService.getAllFoodCategories().then((foodCategories: FoodCategory[]) => {
+  //     console.log(foodCategories)
+  //     setFoodCategories(foodCategories)
+  //   })
+  // }
+
+  // getFoodCategories()
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Categorias</Text>
--- screens/foodCategories/RegisterFoodCategoryScreen.tsx
+++ screens/foodCategories/RegisterFoodCategoryScreen.tsx
@@ -1,10 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { Text, View } from '../../components/Themed';
+import * as databaseService from '../../services/databaseService';
 
 export default function RegisterFoodCategoryScreen() {
+  const [description, setDescription] = useState('');
+
+  async function createFoodCategory(): Promise<void> {
+    if (description && description.trim() != '') {
+      await databaseService.createFoodCategory(description)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Cadastro de categoria</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputLabel}>
+          Descrição
+        </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(value: string) => {
+            setDescription(value)
+          }}
+          value={description}
+        />
+        {
+          !(description && description.trim() != '') ?
+            <Text style={styles.errorText}>
+              Descrição não informada
+            </Text> : null
+        }
+        <View style={styles.marginBottom}></View>
+        <TouchableOpacity style={styles.button}
+          onPress={async () => {
+            await createFoodCategory()
+          }}>
+          <Text style={styles.buttonText}>
+            Criar
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -15,9 +51,40 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainer: {
+    marginBottom: 10,
+  },
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#ddd',
+    minWidth: 250
+  },
+  inputLabel: {
+    fontSize: 18,
+  },
+  button: {
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#040d59',
+  },
+  buttonText: {
+    color: '#FFF',
+  },
+  errorText: {
+    color: '#a00'
+  },
+  marginBottom: {
+    marginBottom: 30
+  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 15
   },
   separator: {
     marginVertical: 30,
